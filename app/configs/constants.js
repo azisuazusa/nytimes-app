f636cfe203138ea8468cf5658f9b3221bc524269
AzisuAzusa <azis.abdul.bachar@gmail.com>
2019-01-15 05:49:02 +0700

    create constants for sort by and search by

---
+++ app/configs/constants.js
@@ -0,0 +1,13 @@
+const sortArticle = [
+  { label: "Sort by...", value: "" },
+  { label: "Newest", value: "newest" },
+  { label: "Oldest", value: "oldest" }
+];
+
+const bookList = [
+  { label: "Search by...", value: "" },
+  { label: "E-Book Fiction", value: "e-book-fiction" },
+  { label: "Hardcover Fiction", value: "hardcover-fiction" }
+];
+
+export default { sortArticle, bookList };
