80b56867f2c68b6a2520301a088ab0da1b067f4d
AzisuAzusa <azis.abdul.bachar@gmail.com>
2019-01-15 05:50:47 +0700

    implement searchbox

--- app/containers/Article/index.js
+++ app/containers/Article/index.js
@@ -3,15 +3,48 @@
  */
 
 import React, { Component } from 'react';
-import { Container } from 'native-base';
+import { Container, Form, Item, Content } from 'native-base';
+import SearchBox from '../../components/Article/SearchBox';
 
 type Props = {};
-type State = {};
+type State = {
+  searchValue: string,
+  sortValue: string
+};
 
 export default class Article extends Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      searchValue: '',
+      sortValue: ''
+    }
+  }
+
+  onChangeSearchValue(searchValue: string) {
+    this.setState({ searchValue });
+  }
+
+  onChangeSortValue(sortValue: string) {
+    this.setState({ sortValue });
+  }
+
   render() {
+    const { searchValue } = this.state;
     return (
       <Container>
+        <Content>
+          <Form>
+            <Item>
+              <SearchBox
+                value={searchValue}
+                onChangeText={this.onChangeSearchValue.bind(this)}
+                onValueChange={this.onChangeSortValue.bind(this)} />
+            </Item>
+          </Form>
+          <Container>
+          </Container>
+        </Content>
       </Container>
     );
   }
