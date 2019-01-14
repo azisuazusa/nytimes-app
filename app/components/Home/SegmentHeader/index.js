18d288087c415bf14c8288a0c8bf544d6024ebd9
AzisuAzusa <azis.abdul.bachar@gmail.com>
2019-01-14 14:53:36 +0700

    create functionality of segment header

--- app/components/Home/SegmentHeader/index.js
+++ app/components/Home/SegmentHeader/index.js
@@ -5,21 +5,30 @@
 import React, { Component } from 'react';
 import { Header, Left, Body, Right, Button, Icon, Segment, Text } from 'native-base';
 
-type Props = {};
+type Props = {
+  active: number,
+  onPressArticle: () => void,
+  onPressBook: () => void
+};
 
 export default class SegmentHeader extends Component<Props> {
+  static defaultProps = {
+    active: 0
+  }
+
   render() {
+    const { active, onPressArticle, onPressBook } = this.props;
     return (
       <Header hasSegment>
-        <Left>
-          <Button transparent>
-            <Icon name="arrow-back" />
-          </Button>
-        </Left>
+        <Left />
         <Body>
           <Segment>
-            <Button first active><Text>Articles</Text></Button>
-            <Button last><Text>Books</Text></Button>
+            <Button first active={active == 0} onPress={() => onPressArticle()}>
+              <Text>Articles</Text>
+            </Button>
+            <Button last active={active == 1} onPress={() => onPressBook()}>
+              <Text>Books</Text>
+            </Button>
           </Segment>
         </Body>
         <Right />
--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -7,12 +7,30 @@ import { Container } from 'native-base';
 import SegmentHeader from '../../components/Home/SegmentHeader';
 
 type Props = {};
+type State = {
+  active: number
+};
 
-export default class Home extends Component<Props> {
+export default class Home extends Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      active: 0
+    }
+  }
+
+  onPressSegment(active: number) {
+    this.setState({ active });
+  }
+
   render() {
+    const { active } = this.state;
     return (
       <Container>
-        <SegmentHeader />
+        <SegmentHeader
+          active={active}
+          onPressArticle={() => this.onPressSegment(0)}
+          onPressBook={() => this.onPressSegment(1)} />
       </Container>
     );
   }
