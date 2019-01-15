ec1707b27177a273d4e88e3b6bac01f056406af6
AzisuAzusa <azis.abdul.bachar@gmail.com>
2019-01-15 10:17:19 +0700

    create articleApi

--- app/apis/article.js
+++ app/apis/article.js
@@ -4,11 +4,11 @@
 import constants from '../configs/constants';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: constants.baseUrl });
-
-const getArticles = (q: string, sort: string) : Promise<Array<any>> => {
+const getArticles = (q: string, sort: string) : Promise<any> => {
   let params = {};
   if (q != '') params.q = q;
   if (sort != '') params.sort = sort;
-  return api.get(constants.articleUrl, { params });
+  return axios.get(constants.articleUrl, { params });
 }
+
+export default { getArticles };
