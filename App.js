f5e5476ac654dbfe62a6e6790a1cdb98721f4b2c
AzisuAzusa <azis.abdul.bachar@gmail.com>
2019-01-15 10:16:47 +0700

    init axios

--- App.js
+++ App.js
@@ -5,12 +5,17 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import Routes from './app/routes';
+import initializations from './app/configs/initializations';
 
 type Props = {};
 
 export default class App extends Component<Props> {
+  constructor() {
+    super();
+    initializations.initAxios();
+  }
+
   render() {
     return <Routes />;
   }
